c84b1c2fe20d0be725e7dfc75709a7b2fadea29e
sadrisso <sadriisso304@gmail.com>
2025-02-06 21:36:21 +0600

    project details and skill details api added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors()); // Enable CORS
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,6 +44,19 @@ async function run() {
             res.send(result)
         })
 
+        app.get("/skill/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await skillsCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get("/project/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await projectsCollection.findOne(query)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
